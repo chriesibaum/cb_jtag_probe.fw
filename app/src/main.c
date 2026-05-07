8e9106b04508e5e091a6fd88708432bd765ca8e6
Thomas Popp <Thomas@chriesibaum.com>
2026-05-07 11:15:15 +0200

    added a status LED
    - raspi dev tree ++
    - udev rules moved to script
    - cleanup

--- app/src/main.c
+++ app/src/main.c
@@ -3,6 +3,7 @@
 #include <zephyr/kernel.h>
 
 #include "error.h"
+#include "sys.h"
 #include "info.h"
 #include "jtag_engine.h"
 #include "usb_bulk_jtag.h"
@@ -16,6 +17,9 @@ LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
 int32_t main(void)
 {
     int ret;
+
+    ret = sys_init();
+    ERR_CHECK(ret < 0, ret, "System init failed (err %d)", ret);
 
     ret = jtag_engine_init();
     ERR_CHECK(ret < 0, ret, "JTAG engine init failed (err %d)", ret);
